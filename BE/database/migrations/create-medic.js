6c31c34aab27f3ae39fd3059808abaaa8df0ef8e
Enzo González <ghrelin00@gmail.com>
2023-08-23 19:26:08 -0300

    feat: add migration file to create medic table
    Added a migration file to create the medic table in the database.

---
+++ BE/database/migrations/create-medic.js
@@ -0,0 +1,64 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Medics', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      fullname: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      resume: {
+        type: Sequelize.STRING
+      },
+      country: {
+        type: Sequelize.STRING
+      },
+      avatar: {
+        type: Sequelize.TEXT
+      },
+      location: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      gender: {
+        type: Sequelize.ENUM('male', 'female', 'other'),
+        allowNull: false
+      },
+      profesionalid: {
+        type: Sequelize.BIGINT,
+      },
+      nid: {
+        type: Sequelize.BIGINT
+      },
+      birthdate: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Medics');
+  }
+};
