bdbd3964bf5db8c9dfe5d76ec605c772b5f27bf1
miguelbel00 <miguelabeltranp@gmail.com>
2023-08-23 15:27:27 -0500

    fix: change password env variable and add port env variable

--- BE/config/config.js
+++ BE/config/config.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 
 module.exports={
-  
+
     "development": {
       "username": process.env.DEV_DB_USER,
-      "password": process.env.DEV_PASSWORD,
+      "password": process.env.DEV_DB_PASSWORD,
       "database": process.env.DEV_DB_NAME,
       "host": process.env.DEV_DB_HOST,
       "dialect": "postgres"
@@ -12,6 +13,7 @@ module.exports={
       "username": process.env.PRO_DB_USER,
       "password": process.env.PRO_DB_PASSWORD,
       "database": process.env.PRO_DB_NAME,
+      "port": process.env.PRO_DB_PORT,
       "host": process.env.PRO_DB_HOST,
       "dialect": "postgres"
     }
