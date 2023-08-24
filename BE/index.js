06332771a3aeda4c60183e78c4c4ea73ae6d049f
David Giraldo <degiraldod@gmail.com>
2023-08-24 11:09:22 -0500

    feat: add route middlewares

--- BE/index.js
+++ BE/index.js
@@ -3,6 +3,9 @@ const app = express();
 const morgan = require('morgan')
 const cors = require('cors')
 
+const patients = require('./routes/patientsRoutes.js')
+const medics = require('./routes/medicsRoutes.js')
+
 // Middleware
 
 app.use(cors())
@@ -10,7 +13,8 @@ app.use(morgan('dev'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
+app.use('/api/v1/', patients)
+app.use('/api/v1/', medics)
 
 app.get('/',(req,res,next)=> {
   return res.send("running").end
@@ -18,7 +22,7 @@ app.get('/',(req,res,next)=> {
 
 //Routes
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
