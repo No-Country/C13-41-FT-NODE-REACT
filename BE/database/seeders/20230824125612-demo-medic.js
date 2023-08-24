07aa3061999e62491ce2a661b17fc834183b24af
David Giraldo <degiraldod@gmail.com>
2023-08-24 11:01:10 -0500

    feat(database): add database seeders

---
+++ BE/database/seeders/20230824125612-demo-medic.js
@@ -0,0 +1,42 @@
+"use strict";
+const bcrypt = require("bcrypt");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    const hashedPwd = await bcrypt.hash("1234", 10);
+    return await queryInterface.bulkInsert("Medics", [
+      {
+        fullname: "David Giraldo",
+        password: hashedPwd,
+        email: "example@example.com",
+        country: "Colombia",
+        gender: "male",
+        birthdate: new Date(),
+        nid: "123456789",
+        profesionalid: "123456789",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    return await queryInterface.bulkDelete("Medics", null, {});
+  },
+};
