15f1cdf27378e73e71277dacc8ba98e388e11836
Enzo González <ghrelin00@gmail.com>
2023-08-25 18:33:31 -0300

    feat(routes): add delete and get routes for medicsRoutes

--- BE/controllers/Consult/index.js
+++ BE/controllers/Consult/index.js
@@ -1,7 +1,9 @@
 const { editConsult } = require("./editConsult");
 const { createConsult } = require("./registerConsult");
+const { deleteConsult } = require("./deleteConsult");
 
 module.exports = {
   editConsult,
   createConsult,
+  deleteConsult
 };
