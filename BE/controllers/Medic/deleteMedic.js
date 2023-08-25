c4ac622c2ee7861f106c76bb567dd34c63882f60
Enzo González <ghrelin00@gmail.com>
2023-08-25 18:16:28 -0300

    feat(controllers): add delete controller for Medic

---
+++ BE/controllers/Medic/deleteMedic.js
@@ -0,0 +1,36 @@
+const { Medic } = require("../../database/models");
+
+// TODO: Cambiar con middleware de atenticación
+const deleteMedic = async (req, res) => {
+  try {
+    const {
+      email,
+    } = req.body;
+
+    if (
+      !email
+    ) {
+      throw new Error("All fields are required")
+    }
+
+    const deletedMedic = await Medic.destroy({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!deletedMedic) {
+      throw new Error("Medic not found")
+    }
+
+    return res
+      .status(200)
+      .json({ message: "medic deleted" });
+  } catch (error) {
+    return res.status(400).json({ message: error.message, error: "Delete Medic" });
+  }
+};
+
+module.exports = {
+  deleteMedic,
+};
