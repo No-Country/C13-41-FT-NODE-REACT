454e08d1e464f4074482f6fd01813e9bf49cd5ce
Maximiliano Pérez <maximiliano.leonel.perez1999@gmail.com>
2023-08-28 18:23:19 -0300

    feat: layout doctors list page with filter by name

--- fe/src/app/layout.jsx
+++ fe/src/app/layout.jsx
@@ -1,6 +1,6 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
-
+import { FilterProvider } from '@/contexts/Filters.context';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
@@ -10,8 +10,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
 	return (
-		<html lang='en'>
-			<body className={inter.className}>{children}</body>
-		</html>
+		<FilterProvider>
+			<html lang='en'>
+				<body className={inter.className}>{children}</body>
+			</html>
+		</FilterProvider>
 	);
 }
