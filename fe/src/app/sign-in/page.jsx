0772ac84ad626701cdaaeffe7bb6268f99ebc53f
Christian Omar <chris_ortiz195@hotmail.com>
2023-08-29 12:06:40 -0600

    feat: add form sign in

--- fe/src/app/sign-in/page.jsx
+++ fe/src/app/sign-in/page.jsx
@@ -1,134 +1,178 @@
-'use client';
-import React, { useState } from 'react';
-import styles from './page.module.css';
-import Link from 'next/link';
-import * as yup from 'yup';
-import { Router } from 'next/router';
+"use client";
+import React, { useState } from "react";
+import styles from "./page.module.css";
+import Link from "next/link";
+import * as yup from "yup";
+import { Formik, Form, Field } from "formik";
+import {
+  Container,
+  Button,
+  Typography,
+  Grid,
+  Snackbar,
+  Alert,
+} from "@mui/material";
+import { Router } from "next/router";
+
 const SignInPage = () => {
-	const [fieldErrors, setFieldErrors] = useState({});
+  const [fieldErrors, setFieldErrors] = useState({});
 
-	// Datos de ejemplo hasta tener la API
-	const successLogin = {
-		data: { patient: 'paciente' },
-		message: 'success login',
-	};
-	const errorLogin = {
-		error: 'duplicated',
-		message: 'email was duplicate',
-	};
+  // Datos de ejemplo hasta tener la API
+  const successLogin = {
+    data: { patient: "paciente" },
+    message: "success login",
+  };
+  const errorLogin = {
+    error: "duplicated",
+    message: "email was duplicate",
+  };
 
-	const validateSignInData = async data => {
-		const signInSchema = yup.object({
-			email: yup.string().email('Invalid email').required('Email is required'),
-			password: yup.string().required('Password is required'),
-			user: yup.string().oneOf(['patient', 'doctor'], 'Select one user').required(''),
-		});
+  const initialValues = {
+    email: "",
+    password: "",
+  };
 
-		await signInSchema.validate(data, { abortEarly: false });
-	};
+  const signInSchema = yup.object({
+    email: yup
+      .string("enter your email")
+      .email("Invalid email")
+      .required("Email is required"),
+    password: yup
+      .string("Enter your password")
+      .min(8, "Password should be of minimum 8 characters length")
+      .required("Password is required"),
+	user: yup
+	  .string()
+	  .oneOf(["patient", "doctor"], "Select one user")
+	  .required(""), 
+  });
 
-	const signIn = async data => {
-		if (!data) return;
 
-		try {
-			await validateSignInData(data);
 
-			let endpoint = '';
-			if (data.user === 'patient') {
-				endpoint = '/api/login/patient';
-			} else {
-				endpoint = '/api/login/doctor';
-			}
+  const onSubmit = (values) => {
+    console.log("values", values);
+  };
 
-			const response = await fetch(endpoint, {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify(data),
-			});
+  return (
+    <Container>
+      <Typography variant="h4" mt={10} mb={10} className={styles.title}>
+        Login
+      </Typography>
+	  <div className={styles.logoContainer}>
+  <img
+  
+    alt="Logo"
+    className={styles.logo}
+  />
+</div>
+      <Link href="/" className={styles.back}>
+        Back
+      </Link>
+      
+        <Formik
+          initialValues={initialValues}
+          validationSchema={signInSchema}
+          onSubmit={onSubmit}
+        >
+          {({ errors, touched, dirty, isValid }) => (
+            <Form>
+              <Grid container alignItems="center" justifyContent="center" paddingY={3} spacing={4} rowSpacing={10}>
+                <Grid item xs={6} md={6}>
+                  <Field 
+				  name="email" 
+				  type="email" 
+				  placeholder="Email" 
+				  style={{ width: '100%', marginBottom: '1rem', padding: '0.5rem', borderRadius: '4px', border: '1px solid #ccc' }} />
+                  {errors.email && touched.email ? (
+                    <div>{errors.email}</div>
+                  ) : null}
 
-			if (response.status === 200) {
-				console.log(successLogin.message);
-				// toast
-				Router.push('/');
-			}
+					<Link href="/forgot-password" className={styles.text}>
+            			Did you Forgot your password?
+          			</Link>
 
-			if (response.status === 404) {
-				console.log(errorLogin.message);
-			}
-		} catch (validationErrors) {
-			const newFieldErrors = {};
-			validationErrors.inner.forEach(error => {
-				newFieldErrors[error.path] = error.message;
-			});
+                  <Field
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+					style={{ width: '100%', marginBottom: '1rem', padding: '0.5rem', borderRadius: '4px', border: '1px solid #ccc' }}
+                  />
+                  {errors.password && touched.password ? (
+                    <div>{errors.password}</div>
+                  ) : null}
 
-			setFieldErrors(newFieldErrors);
-			setTimeout(() => {
-				setFieldErrors({});
-			}, 3000);
-		}
-	};
+                  <h4>Login as</h4>
+				  <div role="group" aria-labelledby="my-radio-group">
+                  <label>
+                    <Field
+                      type="radio"
+                      name="user"
+                      id="patient"
+                      value="patient"
+                    />
+                    PATIENT
+                  </label>
 
-	const handleSubmit = e => {
-		e.preventDefault();
-		const { email, password, user } = e.target;
+                  <label>
+                    <Field 
+					  type="radio" 
+					  name="user" 
+					  value="doctor" 
+					  id="doctor" 
+					  />
+                    DOCTOR
+                  </label>
 
-		const signInData = {
-			email: email.value,
-			password: password.value,
-			user: user.value,
-		};
+				  </div>
+                </Grid>
+              </Grid>
 
-		signIn(signInData);
-	};
+              <Button
+                color="success"
+                variant="contained"
+                fullWidth
+				size='large'
+                type="submit"
+				disabled={!dirty || !isValid}
+              >
+                Submit
+              </Button>
 
-	return (
-		<main className={styles.main}>
-			<Link href='/' className={styles.back}>
-				Back
-			</Link>
-			<div className={styles.container}>
-				<form className={styles.form} onSubmit={handleSubmit}>
-					<h1 className={styles.title}>Login</h1>
-					<div className={styles.logo}>Logo</div>
-					<div className={styles.inputContainer}>
-						<input type='email' className={styles.input} placeholder='Email' name='email' />
-						{fieldErrors.email && <p className={styles.error}>{fieldErrors.email}</p>}
-					</div>
-					<div className={styles.inputContainer}>
-						<input type='password' className={styles.input} placeholder='Password' name='password' />
-						{fieldErrors.password && <p className={styles.error}>{fieldErrors.password}</p>}
-					</div>
-					<div className={styles.inputContainer}>
-						<h4>Login as</h4>
-						<div className={styles.linkContainer}>
-							<div className={styles.radioContainer}>
-								<input type='radio' id='patient' name='user' value='patient' />
-								<label htmlFor='patient' className={styles.label}>
-									Patient
-								</label>
-							</div>
-							<div className={styles.radioContainer}>
-								<input type='radio' id='doctor' name='user' value='doctor' />
-								<label htmlFor='doctor' className={styles.label}>
-									Doctor
-								</label>
-							</div>
-						</div>
-						{fieldErrors.user && <p className={styles.error}>{fieldErrors.user}</p>}
-					</div>
-					<Link href='/forgot-password' className={styles.text}>
-						Forgot your password
-					</Link>
 
-					<button type='submit' className={styles.button}>
-						Log in
-					</button>
-				</form>
-			</div>
-		</main>
-	);
+			  <div className={styles.socialLoginButtons}>
+            <span className={styles.loginSpan}>Login In With</span>
+            <div className={styles.socialButtonContainer}>
+              <Button
+                 className={`${styles.socialButton} ${styles.googleButton}`}
+                type="button"
+               
+              >
+                Google
+              </Button>
+
+              <Button
+             	className={`${styles.socialButton} ${styles.appleButton}`}
+                type="button"
+                
+              >
+                Apple
+              </Button>
+
+              <Button
+               className={`${styles.socialButton} ${styles.facebookButton}`}
+                type="button"
+                
+              >
+                Facebook
+              </Button>
+            </div>
+          </div>
+            </Form>
+          )}
+        </Formik>
+    
+    </Container>
+  );
 };
 
 export default SignInPage;
