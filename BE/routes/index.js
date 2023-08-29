d99703d9c64126b8d9903b2080c1c0edc1deb1c9
Enzo González <ghrelin00@gmail.com>
2023-08-29 15:59:20 -0300

    feat: Add new route for API documentation

--- BE/routes/index.js
+++ BE/routes/index.js
@@ -1,4 +1,8 @@
 const express = require('express')
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
+
 const patientRoutes = require('./patientsRoutes')
 const medicsRoutes = require('./medicsRoutes')
 const consultsRoutes = require('./consultsRoutes')
@@ -10,5 +14,6 @@ router.use('/medic',medicsRoutes)
 router.use('/patient',patientRoutes)
 router.use('/consult',consultsRoutes)
 router.use('/auth',authRoutes)
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = router
