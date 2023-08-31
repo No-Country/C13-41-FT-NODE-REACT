28faa5e7cccacf3935678281a5efcc6cd8a5fcb5
Christian Omar <chris_ortiz195@hotmail.com>
2023-08-31 12:50:36 -0600

    feat: ADD UserValidation, materialUI

--- fe/src/app/sign-in/page.jsx
+++ fe/src/app/sign-in/page.jsx
@@ -3,14 +3,17 @@ import React, { useState } from "react";
 import styles from "./page.module.css";
 import Link from "next/link";
 import * as yup from "yup";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import {
   Container,
   Button,
   Typography,
   Grid,
   Snackbar,
+  Checkbox,
   Alert,
+  TextField,
+  FormControlLabel,
 } from "@mui/material";
 import { Router } from "next/router";
 
@@ -30,6 +33,7 @@ const SignInPage = () => {
   const initialValues = {
     email: "",
     password: "",
+    remember: false,
   };
 
   const signInSchema = yup.object({
@@ -41,16 +45,20 @@ const SignInPage = () => {
       .string("Enter your password")
       .min(8, "Password should be of minimum 8 characters length")
       .required("Password is required"),
-	user: yup
-	  .string()
-	  .oneOf(["patient", "doctor"], "Select one user")
-	  .required(""), 
+    user: yup
+      .string()
+      .oneOf(["patient", "doctor"], "Select one user")
+      .required(""),
   });
 
+  const onSubmit = (values, props) => {
+    console.log(values);
+    setTimeout(() => {
+      props.resetForm();
+      props.setSubmitting(false);
+    }, 2000);
 
-
-  const onSubmit = (values) => {
-    console.log("values", values);
+    console.log(props);
   };
 
   return (
@@ -58,53 +66,82 @@ const SignInPage = () => {
       <Typography variant="h4" mt={10} mb={10} className={styles.title}>
         Login
       </Typography>
-	  <div className={styles.logoContainer}>
-  <img
-	
-    alt="Logo"
-    className={styles.logo}
-  />
-</div>
-      <Link href="/" className={styles.back}>
-        Back
-      </Link>
-      
-        <Formik
-          initialValues={initialValues}
-          validationSchema={signInSchema}
-          onSubmit={onSubmit}
-        >
-          {({ errors, touched, dirty, isValid }) => (
-            <Form>
-              <Grid container alignItems="center" justifyContent="center" paddingY={3} spacing={4} rowSpacing={10}>
-                <Grid item xs={6} md={6}>
-                  <Field 
-				  name="email" 
-				  type="email" 
-				  placeholder="Email" 
-				  style={{ width: '100%', marginBottom: '1rem', padding: '0.5rem', borderRadius: '4px', border: '1px solid #ccc' }} />
-                  {errors.email && touched.email ? (
-                    <div>{errors.email}</div>
-                  ) : null}
+      <div className={styles.logoContainer}>
+        <img alt="Logo" className={styles.logo} />
+      </div>
+      <Typography>
+        <Link href="/" className={styles.back}>
+          Back
+        </Link>
+      </Typography>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={signInSchema}
+        onSubmit={onSubmit}
+      >
+        {({ errors, touched, dirty, isValid, isSubmitting }) => (
+          <Form>
+            <Grid
+              container
+              alignItems="center"
+              justifyContent="center"
+              paddingY={3}
+              spacing={4}
+              rowSpacing={10}
+            >
+              <Grid item xs={6} md={6}>
+                <Field
+                  as={TextField}
+                  name="email"
+                  type="email"
+                  placeholder="Email"
+                  helperText={<ErrorMessage name="email" />}
+                  style={{
+                    width: "100%",
+                    marginBottom: "1rem",
+                    padding: "0.5rem",
+                    borderRadius: "4px",
+                    border: "1px solid #ccc",
+                  }}
+                />
+                {errors.email && touched.email ? (
+                  <div>{errors.email}</div>
+                ) : null}
+                <Typography>
+                  <Link href="/forgot-password" className={styles.text}>
+                    Did you Forgot your password?
+                  </Link>
+                </Typography>
+                <Field
+                  as={TextField}
+                  type="password"
+                  name="password"
+                  helperText={<ErrorMessage name="password" />}
+                  placeholder="Password"
+                  style={{
+                    width: "100%",
+                    marginBottom: "1rem",
+                    padding: "0.5rem",
+                    borderRadius: "4px",
+                    border: "1px solid #ccc",
+                  }}
+                />
+                {errors.password && touched.password ? (
+                  <div>{errors.password}</div>
+                ) : null}
 
-					<Link href="/forgot-password" className={styles.text}>
-            			Did you Forgot your password?
-          			</Link>
+                <Field
+                  as={FormControlLabel}
+                  name="remember"
+                  control={<Checkbox color="primary"></Checkbox>}
+                  label="Remember Me"
+                ></Field>
 
-                  <Field
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-					style={{ width: '100%', marginBottom: '1rem', padding: '0.5rem', borderRadius: '4px', border: '1px solid #ccc' }}
-                  />
-                  {errors.password && touched.password ? (
-                    <div>{errors.password}</div>
-                  ) : null}
-
-                  <h4>Login as</h4>
-				  <div role="group" aria-labelledby="my-radio-group">
+                <h4>Login as</h4>
+                <div role="group" aria-labelledby="my-radio-group">
                   <label>
                     <Field
+                      as={Checkbox}
                       type="radio"
                       name="user"
                       id="patient"
@@ -114,63 +151,58 @@ const SignInPage = () => {
                   </label>
 
                   <label>
-                    <Field 
-					  type="radio" 
-					  name="user" 
-					  value="doctor" 
-					  id="doctor" 
-					  />
+                    <Field
+                      as={Checkbox}
+                      type="radio"
+                      name="user"
+                      value="doctor"
+                      id="doctor"
+                    />
                     DOCTOR
                   </label>
-
-				  </div>
-                </Grid>
+                </div>
               </Grid>
+            </Grid>
 
-              <Button
-                color="success"
-                variant="contained"
-                fullWidth
-				size='large'
-                type="submit"
-				disabled={!dirty || !isValid}
-              >
-                Submit
-              </Button>
+            <Button
+              color="success"
+              variant="contained"
+              fullWidth
+              size="large"
+              type="submit"
+              disabled={!dirty || !isValid || isSubmitting}
+            >
+              {isSubmitting ? "Loading" : "sign in"}
+            </Button>
 
+            <div className={styles.socialLoginButtons}>
+              <span className={styles.loginSpan}>Login In With</span>
+              <div className={styles.socialButtonContainer}>
+                <Button
+                  className={`${styles.socialButton} ${styles.googleButton}`}
+                  type="button"
+                >
+                  Google
+                </Button>
 
-			  <div className={styles.socialLoginButtons}>
-            <span className={styles.loginSpan}>Login In With</span>
-            <div className={styles.socialButtonContainer}>
-              <Button
-                 className={`${styles.socialButton} ${styles.googleButton}`}
-                type="button"
-               
-              >
-                Google
-              </Button>
+                <Button
+                  className={`${styles.socialButton} ${styles.appleButton}`}
+                  type="button"
+                >
+                  Apple
+                </Button>
 
-              <Button
-             	className={`${styles.socialButton} ${styles.appleButton}`}
-                type="button"
-                
-              >
-                Apple
-              </Button>
-
-              <Button
-               className={`${styles.socialButton} ${styles.facebookButton}`}
-                type="button"
-                
-              >
-                Facebook
-              </Button>
+                <Button
+                  className={`${styles.socialButton} ${styles.facebookButton}`}
+                  type="button"
+                >
+                  Facebook
+                </Button>
+              </div>
             </div>
-          </div>
-            </Form>
-          )}
-        </Formik>
-    
+          </Form>
+        )}
+      </Formik>
     </Container>
   );
 };
