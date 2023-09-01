9753fc3f051bf075d4911a4a885d77aa3ef4b26d
scarolayton <scarolayton@gmail.com>
2023-09-01 17:37:14 -0500

    feat: adding some styles to the calendar

--- fe/Components/Calendar/DoctorCalendar.jsx
+++ fe/Components/Calendar/DoctorCalendar.jsx
@@ -2,9 +2,8 @@ import * as React from 'react';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { Container, display } from '@mui/system';
 import { CardHeader, Grid, IconButton, Typography } from '@mui/material';
-import { ColorsKlinik } from '@/app/colors';
+import { colors } from '@/app/colors';
 import CloseIcon from '@mui/icons-material/Close';
 import { Card } from '@mui/material';
 import addDays from 'date-fns/addDays';
@@ -31,7 +30,7 @@ export default function DoctorCalendar(props) {
   return (
     <Grid  mt={4} mb={10} container gap={4} spacing={2} justifyContent='center' alignItems='center'>
       <Grid item xs={10} md={10}>
-        <Typography color={ColorsKlinik.text} fontSize={18}>Aqui puedes añadir algún dia en especifico en el cual no estaras disponible(recuerda que tiene que ser 5 días antes)</Typography>
+        <Typography color={colors.text} fontSize={18}>Aqui puedes añadir algún dia en especifico en el cual no estaras disponible(recuerda que tiene que ser 5 días antes)</Typography>
       </Grid>
       <Grid item md={4}>
         <LocalizationProvider dateAdapter={AdapterDateFns}>
@@ -43,7 +42,7 @@ export default function DoctorCalendar(props) {
         {values.map((value) => (
                   <Card sx={{margin: 5}}>
                   <CardHeader
-                    titleTypographyProps={{variant:'h7' , color: ColorsKlinik.text } }
+                    titleTypographyProps={{variant:'h7' , color: colors.text } }
                     title={value.title}
                     action={
                       <IconButton onClick={() => handleDeleteDate(value.id)}>
