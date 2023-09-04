4d677c7c1bc8b5a62a145f6ed5cad06c54f652a5
Enzo González <ghrelin00@gmail.com>
2023-09-04 18:56:52 -0300

    feat: add random password generator and email notification

--- BE/controllers/Medic/changePasswordMedic.js
+++ BE/controllers/Medic/changePasswordMedic.js
@@ -1,17 +1,20 @@
 const bcrypt = require("bcrypt");
 const { Medic } = require("../../database/models");
+const {sendEmail} = require('../Email/sendEmail')
+const generatePassword = require('../../helpers/passwordGenerator')
 
 // TODO: Cambiar con middleware de atenticación
 const changePasswordMedic = async (req, res) => {
   try {
 
-    const { email, password } = req.body
+    const { email } = req.body
 
-    if(!email || !password)
+    if(!email)
     {
-      throw new Error("Must contain email and password")
+      throw new Error("Must contain email")
     }
-      
+    
+    const password = generatePassword()
     const hashedPwd = await bcrypt.hash(password, 10)
 
     const updatedMedic = await Medic.update(
@@ -37,9 +40,15 @@ const changePasswordMedic = async (req, res) => {
       }
     );
 
-    return res
-      .status(200)
-      .json({ data:{MedicFound},message: "Medic Updated" });
+    const emailSubject = 'Password reset (Klinika Mecharcovz)'
+
+    const emailBody = `<h1>Your new password is: ${password}</h1>,
+    
+    Best regards,
+    Klinika Mercharcovz`;
+    
+    return sendEmail(res, email, emailSubject, emailBody, 'Email notification for password reset sent')
+
   } catch (error) {
     return res.status(400).json({ message: error.message, error: "Edit Medic" });
   }
