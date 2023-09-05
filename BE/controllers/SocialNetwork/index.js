447e4fd000ccfe3cc6320a3d87ddf7d465d48089
Enzo González <ghrelin00@gmail.com>
2023-09-05 17:02:43 -0300

    feat: add index for controllers

---
+++ BE/controllers/SocialNetwork/index.js
@@ -0,0 +1,7 @@
+const { registerSocialNetwork } = require("./registerSocialNetwork");
+const { editSocialNetwork } = require("./editSocialNetwork");
+
+module.exports = {
+  registerSocialNetwork,
+  editSocialNetwork
+};
