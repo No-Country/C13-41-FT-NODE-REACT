2b079ca54e29a99bd86884a8a68767f9b3f49c50
miguelbel00 <miguelabeltranp@gmail.com>
2023-09-07 14:44:42 -0500

    fix: delete Schedule type attribute

--- BE/controllers/Schedule/createSchedule.js
+++ BE/controllers/Schedule/createSchedule.js
@@ -1,9 +1,9 @@
 const { Schedule } = require('../../database/models')
 const createSchedule = async (req, res) => {
   try {
-    const { day, initialHour, finalHour, type, status, duration, medicId } = req.body
+    const { day, initialHour, finalHour, status, duration, medicId } = req.body
 
-    if (!day || !initialHour || !finalHour || !type || !status || !duration || !medicId) {
+    if (!day || !initialHour || !finalHour || !status || !duration || !medicId) {
       throw new Error('Body must contain day, initialHour, finalHour, type, status, duration, medicId')
     }
 
@@ -11,7 +11,6 @@ const createSchedule = async (req, res) => {
         day,
         initialHour,
         finalHour,
-        type,
         status,
         duration,
         medicId
