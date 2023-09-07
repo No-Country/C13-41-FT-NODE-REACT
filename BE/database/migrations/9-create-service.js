d3034721df9635a6e8acb67f9e72934951f91f98
Enzo González <ghrelin00@gmail.com>
2023-09-07 20:09:52 -0300

    feat: add migration to create service

---
+++ BE/database/migrations/9-create-service.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const { sequelize } = require('../models');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Services', {
+      id: {
+        type: Sequelize.BIGINT,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      price: {
+        type: Sequelize.DOUBLE,
+        allowNull: false
+      },
+      medicId: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: { model: 'Medics', key: 'id'}
+      },
+      specialtyId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'MedicSpecialties', key: 'id'}
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Services');
+  }
+};
