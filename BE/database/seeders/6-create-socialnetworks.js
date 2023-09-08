10aec2d99d20c9e7af022ff57ff7eb309aed6781
Enzo González <ghrelin00@gmail.com>
2023-09-08 17:24:47 -0300

    feat: optimize perfomance

--- BE/database/seeders/6-create-socialnetworks.js
+++ BE/database/seeders/6-create-socialnetworks.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const { Medic, Patient } = require('../models');
+const { Medic } = require('../models');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
@@ -16,37 +16,18 @@ module.exports = {
       "https://www.youtube.com/c/",
     ];
 
-    const usedLinks = new Set();
-
-    for (let i = 0; i < 120; i++) {
-      const row = Math.floor(Math.random() * medics.length);
-      const medic = medics[row];
-      const medic_id = medic.id;
-
-      for (let b = 0; b < 2; b++) {
-        let link;
-        let retries = 0;
-
-        do {
-          if (retries >= links_url.length) {
-            break;
-          }
-
-          const randomLinkIndex = Math.floor(Math.random() * links_url.length);
-          link = `${links_url[randomLinkIndex]}${medic.fullname}`;
-          retries++;
-        } while (usedLinks.has(link));
-
-        usedLinks.add(link);
-
+    medics.forEach((medic) => {
+      links_url.forEach((link) => {
+        const generatedLink = `${link}${medic.fullname}`;
         links.push({
-          medicId: medic_id,
-          link: link,
+          medicId: medic.id,
+          link: generatedLink,
           createdAt: new Date(),
           updatedAt: new Date(),
         });
-      }
-    }
+      });
+    });
+      
     return await queryInterface.bulkInsert("SocialNetworks", links);
   },
   async down(queryInterface, Sequelize) {
