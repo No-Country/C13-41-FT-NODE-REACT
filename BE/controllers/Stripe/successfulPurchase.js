f4a1f25e76084f559303e1d126c696e06889b44c
miguelbel00 <miguelabeltranp@gmail.com>
2023-09-11 14:07:42 -0500

    change success and cancel routes

--- BE/controllers/Stripe/successfulPurchase.js
+++ BE/controllers/Stripe/successfulPurchase.js
@@ -19,7 +19,7 @@ const successfulPurchase = async (req, res) => {
     /* await sendEmail(res, medicEmail, `Invoice #${invoiceId} Update`, templateInvoice(invoiceId,'Accepted',medicFullname))
     await sendEmail(res, patientEmail, `Invoice #${invoiceId} Update`, templateInvoice(invoiceId,'Accepted',patientFullname)) */
 
-    return res.redirect(`${process.env.NODE_ENV == "development" ? process.env.DEV_STRIPE_FE_PAGE_SUCCESS : process.env.PRO_STRIPE_FE_PAGE_SUCCESS}`);
+    return res.redirect(`${process.env.NODE_ENV == "development" ? process.env.DEV_STRIPE_FE_PAGE_SUCCESS : process.env.PRO_STRIPE_FE_PAGE_SUCCESS}?result=success`);
   } catch (error) {
     return res.status(400).json({ message: error.message, error: "Success Stripe" });
   }
