37dfea40a9039457e53adfdc17abd350f67f5bda
Enzo González <ghrelin00@gmail.com>
2023-09-14 19:53:51 -0300

    feat: add paginator to getPatient

--- BE/controllers/Patient/getPatient.js
+++ BE/controllers/Patient/getPatient.js
@@ -1,4 +1,4 @@
-const { Patient, Comment, Consult, Invoice } = require('../../database/models')
+const { Patient, Comment, Consult, Invoice } = require('../../database/models');
 
 const modelsToInclude = [
   { model: Comment, as: 'comments' },
@@ -6,25 +6,49 @@ const modelsToInclude = [
   { model: Invoice, as: 'invoices' }
 ];
 
+const rowsPerPage = 15; // Number of rows per page
+
 const getPatient = async (req, res) => {
-    try {
-      const { email } = req.query;
-  
-      const patient = email
-      ?
-      (await Patient.findOne({
-        where: {
-          email: email
-        },include: modelsToInclude
-      }))
-      : await Patient.findAll({include:modelsToInclude})
+  try {
+    const { email } = req.query;
 
-      return res.status(200).json({ message: 'Patient data', data:{patient} })
-  
-    } catch (error) {
+    if (email) {
+      // If email is provided, retrieve a single patient by email
+      const patient = await Patient.findOne({
+        where: { email },
+        include: modelsToInclude
+      });
 
-      return res.status(400).json({ error: 'Get Patient', message:error.message })
+      if (!patient) {
+        return res.status(404).json({ message: 'Patient not found' });
+      }
+
+      return res.status(200).json({ message: 'Patient data', data: { patient } });
     }
-  }
 
-  module.exports = {getPatient}
+    // If email is not provided, retrieve all patients
+    const patients = await Patient.findAll({
+      include: modelsToInclude
+    });
+
+    // Organize patients into pages
+    const pages = [];
+    for (let i = 0; i < patients.length; i += rowsPerPage) {
+      const pagePatients = patients.slice(i, i + rowsPerPage);
+      pages.push(pagePatients);
+    }
+
+    return res.status(200).json({
+      message: 'Patient data',
+      data: {
+        patients: {
+          page: pages,
+        },
+      },
+    });
+  } catch (error) {
+    return res.status(400).json({ error: 'Get Patient', message: error.message });
+  }
+};
+
+module.exports = { getPatient };
